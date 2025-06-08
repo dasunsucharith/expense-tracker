57d4bc1291142b1a6f8b2a00b917c2bf41814315
Dasun Sucharith <sucharith.dasun@gmail.com>
2025-06-08 22:52:48 +0530

    Handle JWT errors specifically

--- src/routes/user.py
+++ src/routes/user.py
@@ -29,7 +29,9 @@ def token_required(f):
         try:
             data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
             current_user = User.query.filter_by(id=data['user_id']).first()
-        except:
+        except jwt.ExpiredSignatureError:
+            return jsonify({'message': 'Token has expired!'}), 401
+        except jwt.InvalidTokenError:
             return jsonify({'message': 'Token is invalid!'}), 401
             
         return f(current_user, *args, **kwargs)
